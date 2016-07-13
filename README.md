c4a3c847c9e6ea095e113d64a2938ca6c94b86a8
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-07-14 02:56:31 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Falcon example
+
+Personal memo for Falcon.
+
+# Reference
+- [Falcon](https://falconframework.org/)
+- [github.com/falconry/falcon](https://github.com/falconry/falcon)
+- [Qiita article by yohjizzz](http://qiita.com/yohjizzz/items/f46bb3bc7b7c40768836)
---
+++ server.py
@@ -0,0 +1,67 @@
+import json
+import falcon
+
+
+class Middleware(object):
+    def process_request(self, req, resp):
+        print "process_request called"
+
+    def process_resource(self, req, resp, resource, param):
+        print "process_resource called", resource, param
+
+    def process_response(self, req, resp, resource):
+        print "process_response called", resource
+
+
+def before(before_param):
+    def hook(req, resp, resource, params):
+        print "before called", resource
+
+    return hook
+
+
+def after(after_param):
+    def hook(req, resp, resource):
+        print "after called", resource
+
+    return hook
+
+
+class CommentsResource(object):
+
+    @falcon.before(before("get beofre param"))
+    @falcon.after(after("get after param"))
+    def on_get(self, req, resp):
+        comments = self._get_comments()
+        resp.body = json.dumps(comments)
+
+    @falcon.before(before("post before param"))
+    @falcon.after(after("post after param"))
+    def on_post(self, req, resp):
+        comments = self._get_comments()
+        body = req.stream.read()
+        print(body)
+        new_comments = json.loads(body)
+        comments.append(new_comments)
+        self._write_comments(comments)
+
+    def _get_comments(self):
+        with open('comments.json', 'r') as f:
+            comments = json.loads(f.read())
+
+        return comments
+
+    def _write_comments(self, comments):
+        with open('comments.json', 'w') as f:
+            f.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+
+
+app = falcon.API(middleware=Middleware())
+app.add_route("/api/comments", CommentsResource())
+
+
+if __name__ == "__main__":
+    from wsgiref import simple_server
+
+    httpd = simple_server.make_server("127.0.0.1", 3001, app)
+    httpd.serve_forever()
